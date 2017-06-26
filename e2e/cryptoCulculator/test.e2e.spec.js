9f81a2436a8ca60d208335e0b3782959fc4fd4f7
nomid0612 <nomid0612@gmail.com>
2017-06-26 09:53:42 +0300

    Update test.e2e.spec.js

--- e2e/cryptoCulculator/test.e2e.spec.js
+++ e2e/cryptoCulculator/test.e2e.spec.js
@@ -3,7 +3,7 @@ const cryptoCulc = require('./cryptoCulc');
 describe('Crypto Caclulator ', () => {
 
     it('should verify correct counting per month', () => {
-        cryptoCulc.calculation(30, 100, 'TH/s', 1000, 1,2);
+        cryptoCulc.calculation(30, 100, 'TH/s', 1000, 1);
     });
 
 });
